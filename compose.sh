d4bd4213658b0b45a1a2428717d2ee7533e620de
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-05-24 14:08:34 -0300

    :art: Improve "compose.sh"

--- compose.sh
+++ compose.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 if [ "${1}" = '-h' ]; then
-  echo "${0} [any command]"
+  echo "${0} [compose args]"
   exit 0
 fi
 
-docker-compose run --rm server ${@}
+COMMAND='docker-compose'
+
+if [ "${1}" = 'run' ]; then
+  shift
+  COMMAND="${COMMAND} run --rm"
+fi
+
+${COMMAND} ${@}
