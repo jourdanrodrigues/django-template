e3c4b2cd6838aa8e4bcc5b6aa8748a997566b228
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-05-26 00:35:11 -0300

    :umbrella: Add a request in the test

--- core/tests.py
+++ core/tests.py
@@ -2,11 +2,16 @@ from threading import Thread
 from unittest import TestCase
 from wsgiref.simple_server import make_server
 
+import requests
+
 from core.wsgi import application
 
 
 class TestServer(TestCase):
     def test_that_server_turns_on_ok_through_wsgi(self):
-        httpd = make_server('', 8912, application)
-        Thread(target=httpd.serve_forever).start()  # Start in a thread because it blocks the execution
-        httpd.shutdown()
+        port = 8912
+        server = make_server('', port, application)
+        Thread(target=server.serve_forever).start()  # Start in a thread because it blocks the execution
+        response = requests.get('http://localhost:{}/'.format(port))  # Shouldn't raise exception
+        assert response.status_code
+        server.shutdown()
