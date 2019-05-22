49b80d0ca2cb45bd39ea49791b49041da671f707
Jourdan Rodrigues <thiagojourdan@gmail.com>
2019-05-22 11:50:33 -0300

    :sparkles: Add "manage.py" shortcut to "compose.sh"

--- compose.sh
+++ compose.sh
@@ -2,16 +2,21 @@
 
 CURRENT_DIR=$(dirname ${0})
 
-if [ "${1}" == 'dev' ]; then
+if [[ "${1}" == 'dev' ]]; then
   shift
   COMMAND="docker-compose -f docker-compose.yml -f docker-compose.dev.yml"
 else
   COMMAND="docker-compose"
 fi
 
-if [ "${1}" == 'run' ]; then
+if [[ "${1}" == 'run' ]]; then
   shift
   COMMAND="${COMMAND} run --rm"
+fi
+
+if [[ "${1}" == 'manage.py' ]]; then
+  shift
+  COMMAND="${COMMAND} server python manage.py"
 fi
 
 ${COMMAND} ${@}
