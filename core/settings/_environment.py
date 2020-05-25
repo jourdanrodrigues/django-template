5159f152752af1ee7a5ff716a77dfa582d3ad72f
Jourdan Rodrigues <thiagojourdan@gmail.com>
2020-05-24 22:26:48 -0300

    :snowflake: Make boolean environment variables JSON values

--- core/settings/_environment.py
+++ core/settings/_environment.py
@@ -1,6 +1,13 @@
+import json
 import os
 
 from core.helpers import DotEnvReader
+
+
+def bool_env(key, default: bool) -> bool:
+    value = os.getenv(key)
+    return json.loads(value) if value in ['true', 'false'] else default
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -8,7 +15,7 @@ DotEnvReader(os.path.join(BASE_DIR, '.env')).read()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-PRODUCTION = bool(int(os.getenv('PRODUCTION', 0)))
-DEBUG = bool(int(os.getenv('DEBUG', not PRODUCTION)))
+PRODUCTION = bool_env('PRODUCTION', False)
+DEBUG = bool_env('DEBUG', not PRODUCTION)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '' if PRODUCTION else '*').split(',')
